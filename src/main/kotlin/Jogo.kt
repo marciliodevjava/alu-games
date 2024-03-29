c0878949376840cc8660026edcc1ccd70b344317
marciliodevjava <marciliodevjava@gmail.com>
2024-03-28 21:03:28 -0300

    Update Jogo

--- src/main/kotlin/Jogo.kt
+++ src/main/kotlin/Jogo.kt
@@ -3,10 +3,10 @@ class Jogo(val titulo: String, val capa: String, informacao: String) {
     val descricao = informacao
 
     override fun toString(): String {
-        return "Meu Jogo: \n" +
+        return "\nMeu Jogo: \n" +
                 "Titulo: $titulo \n" +
                 "Capa:  $capa \n" +
-                "Descricao: $descricao"
+                "Descricao: $descricao\n"
     }
 
 }
