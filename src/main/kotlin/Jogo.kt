8816c3bc71b4a7c5914769319b47a7abc453c51c
marciliodevjava <marciliodevjava@gmail.com>
2024-03-29 20:05:17 -0300

    Update Jogo

--- src/main/kotlin/Jogo.kt
+++ src/main/kotlin/Jogo.kt
@@ -1,4 +1,10 @@
-class Jogo(val titulo: String, val capa: String, informacao: String) {
+import com.google.gson.annotations.SerializedName
+
+class Jogo(
+    @SerializedName("title") val titulo: String,
+    @SerializedName("thumb") val capa: String,
+    @SerializedName("steamAppID") var informacao: String
+) {
 
     val descricao = informacao
 
