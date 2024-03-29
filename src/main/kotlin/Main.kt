8a64eadd30cd18ce9284d33e1d6328abf84f4417
marciliodevjava <marciliodevjava@gmail.com>
2024-03-28 21:03:40 -0300

    Update Main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,4 @@
+import com.google.gson.Gson
 import java.net.URI
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
@@ -11,7 +12,24 @@ fun main(args: Array<String>) {
         .build()
 
     val response = client.send(request, BodyHandlers.ofString())
-
     var json = response.body()
-    println(json)
+
+    //   print(json)
+    val gson = Gson();
+
+    val meuJogo = Jogo(
+        "Batman: Arkham Asylum Game of the Year Edition",
+        "https:\\/\\/cdn.cloudflare.steamstatic.com\\/steam\\/apps\\/35140\\/capsule_sm_120.jpg?t=1702934705",
+        "Jogo de PC"
+    )
+
+    print(meuJogo)
+    print("                      ")
+    val novoJogo = Jogo(
+        capa = "https:\\/\\/cdn.cloudflare.steamstatic.com\\/steam\\/apps\\/35140\\/capsule_sm_120.jpg?t=1702934705",
+        informacao = "Jogo de PC",
+        titulo = "Batman: Arkham Asylum Game of the Year Edition"
+    )
+
+    print(novoJogo)
 }
