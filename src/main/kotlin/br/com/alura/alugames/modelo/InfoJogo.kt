992175714443659031ae3e1811b51b12550d9eed
marciliodevjava <marciliodevjava@gmail.com>
2024-04-01 10:57:31 -0300

    Update InfoJogo

--- src/main/kotlin/br/com/alura/alugames/modelo/InfoJogo.kt
+++ src/main/kotlin/br/com/alura/alugames/modelo/InfoJogo.kt
@@ -1,3 +1,7 @@
+package br.com.alura.alugames.modelo
+
+import br.com.alura.alugames.modelo.InfoApiShark
+
 data class InfoJogo(val info: InfoApiShark) {
     override fun toString(): String {
         return info.toString()
