d33cbaa9ee8c61ba14d83f300efe7cf125b070bf
marciliodevjava <marciliodevjava@gmail.com>
2024-04-01 11:08:59 -0300

    Update ConsulmoApi

---
+++ src/main/kotlin/br/com/alura/alugames/servicos/ConsulmoApi.kt
@@ -0,0 +1,26 @@
+package br.com.alura.alugames.servicos
+
+import br.com.alura.alugames.modelo.InfoJogo
+import com.google.gson.Gson
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
+
+class ConsulmoApi {
+
+    fun buscarApi(id: String): InfoJogo {
+        val endereco = "https://www.cheapshark.com/api/1.0/games?id=$id"
+        val client: HttpClient = HttpClient.newHttpClient()
+        val request = HttpRequest.newBuilder()
+            .uri(URI.create(endereco))
+            .build()
+
+
+        val response = client.send(request, HttpResponse.BodyHandlers.ofString())
+        var json = response.body()
+        val gson = Gson()
+        val meuInfoJogo = gson.fromJson(json, InfoJogo::class.java)
+        return meuInfoJogo
+    }
+}
