31ee8fa2fa1e659e212ed7537814e51d1e05563c
marciliodevjava <marciliodevjava@gmail.com>
2024-04-01 11:08:49 -0300

    Update Main

--- src/main/kotlin/br/com/alura/alugames/principal/Main.kt
+++ src/main/kotlin/br/com/alura/alugames/principal/Main.kt
@@ -1,12 +1,7 @@
 package br.com.alura.alugames.principal
 
-import br.com.alura.alugames.modelo.InfoJogo
 import br.com.alura.alugames.modelo.Jogo
-import com.google.gson.Gson
-import java.net.URI
-import java.net.http.HttpClient
-import java.net.http.HttpRequest
-import java.net.http.HttpResponse.BodyHandlers
+import br.com.alura.alugames.servicos.ConsulmoApi
 import java.util.*
 
 
@@ -15,33 +10,13 @@ fun main(args: Array<String>) {
     val leitura = Scanner(System.`in`)
     print("Digite um id para buscar: ")
     val id = leitura.nextLine()
-    val endereco = "https://www.cheapshark.com/api/1.0/games?id=$id"
-    val client: HttpClient = HttpClient.newHttpClient()
-    val request = HttpRequest.newBuilder()
-        .uri(URI.create(endereco))
-        .build()
 
-    val response = client.send(request, BodyHandlers.ofString())
-
-//    try {
-//        var json = response.body()
-//        val gson = Gson()
-//        val meuInfoJogo = gson.fromJson(json, br.com.alura.alugames.modelo.InfoJogo::class.java)
-//        val meuJogo = br.com.alura.alugames.modelo.Jogo(
-//            meuInfoJogo.info.title, meuInfoJogo.info.thumb, meuInfoJogo.info.steamAppID
-//        )
-//        print(meuJogo)
-//    } catch (ex: Exception) {
-//        print("br.com.alura.alugames.modelo.Jogo não encontrado. Tente outro id.")
-//    }
-
+    val buscarApi = ConsulmoApi()
+    val informacaoJogo = buscarApi.buscarApi(id)
     var meuJogo: Jogo? = null
 
     val resultado = runCatching {
-        var json = response.body()
-        val gson = Gson()
-        val meuInfoJogo = gson.fromJson(json, InfoJogo::class.java)
-        meuJogo = Jogo(meuInfoJogo.info.title, meuInfoJogo.info.thumb, meuInfoJogo.info.steamAppID)
+        meuJogo = Jogo(informacaoJogo.info.title, informacaoJogo.info.thumb, informacaoJogo.info.steamAppID)
         print(meuJogo)
     }
 
