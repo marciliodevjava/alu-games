4bd8c0f5c96de17d26c8bb0dfe40fda8cca55338
marciliodevjava <marciliodevjava@gmail.com>
2024-04-01 11:29:08 -0300

    Create TesteGamer

---
+++ src/main/kotlin/br/com/alura/alugames/principal/TesteGamer.kt
@@ -0,0 +1,25 @@
+import br.com.alura.alugames.modelo.Gamer
+
+fun main() {
+    val gamer1 = Gamer(
+        "Jacque",
+        "jacque@email.com"
+    )
+    print(gamer1)
+
+    val gamer2 = Gamer(
+        "Jeni",
+        "jeni@email.com",
+        "19/19/1992",
+        "jeniblo"
+    )
+    print(gamer2)
+
+    gamer1.let {
+        it.dataNascimento = "18/09/2000"
+        it.usuario = "jacqueskywalker"
+        it.idInterno = "jacqueskywalker1234"
+    }
+
+    print(gamer1)
+}
