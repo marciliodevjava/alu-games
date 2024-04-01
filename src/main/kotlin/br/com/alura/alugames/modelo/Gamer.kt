956a47de31b4913139cc73dd53d3827e8013a2aa
marciliodevjava <marciliodevjava@gmail.com>
2024-04-01 16:48:34 -0300

    Update Gamer

--- src/main/kotlin/br/com/alura/alugames/modelo/Gamer.kt
+++ src/main/kotlin/br/com/alura/alugames/modelo/Gamer.kt
@@ -22,6 +22,13 @@ data class Gamer(var nome: String, var email: String) {
         criarIdInterno()
     }
 
+    init {
+        if (nome.isNullOrBlank()) {
+            throw IllegalArgumentException("Nome está em branco.")
+        }
+        this.email = validarEmail()
+    }
+
     override fun toString(): String {
         return "Gamer(nome='$nome', email='$email', dataNascimento=$dataNascimento, usuario=$usuario, idInterno=$idInterno)\n"
     }
@@ -32,5 +39,13 @@ data class Gamer(var nome: String, var email: String) {
         idInterno = "$usuario#$tag"
     }
 
+    fun validarEmail(): String {
+        val regex = Regex("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$")
+        if (regex.matches(email)) {
+            return email
+        } else {
+            throw IllegalArgumentException("email inválido.")
+        }
+    }
 
 }
